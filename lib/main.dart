0e60f55b4a299923e5a23553f43004bfa5647df9
zundalekzbynek <zbynek.zundalek@gmail.com>
2024-04-03 19:10:33 +0900

    Setup ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
