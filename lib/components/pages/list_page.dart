d41108e2d9d18bdc53c84d785b732ae6f5b2e07d
zundalekzbynek <zbynek.zundalek@gmail.com>
2024-04-04 09:21:03 +0900

    Add a list page

---
+++ lib/components/pages/list_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example list page'),
+      ),
+      body: const Text('This is a list page'),
+    );
+  }
+}
